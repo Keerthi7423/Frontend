7061ee35ba29254e7e9e8cca8d0ff7a9dc394eac
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-05-02 09:33:54 +0530

    React-routing

---
+++ day-11/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>I am learning React and becoming a frontend developer.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ day-11/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Email: keerthi@gmail.com</p>
+    </div>
+  );
+}
+
+export default Contact;
